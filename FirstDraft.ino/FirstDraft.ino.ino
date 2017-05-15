8bb118d5cfdf2253cf91a9deb95290381b761a16
willcruse <willcruse123@gmail.com>
2017-05-15 20:33:43 +0100

    Commity

--- FirstDraft.ino/FirstDraft.ino.ino
+++ FirstDraft.ino/FirstDraft.ino.ino
@@ -1,45 +1,47 @@
 #include <SoftwareSerial.h>
-#define rxPin 0
-#define txPin 1
+#define rxPin 12
+#define txPin 13
 
 SoftwareSerial sSerial(rxPin, txPin);
 int data;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("RFID READ TEST");
   sSerial.begin(9600); //used same baud rate for both. The baud rate is the rate of impulses per second data is transmitted.
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
 }
 
 void loop() {
-  if(sSerial.available() > 0) {
+  sSerial.print("!RW");
+  sSerial.write(byte(0x02));
+  sSerial.write(byte(32));
+  if (sSerial.available() > 0) {
     data = sSerial.read();
-    if (data != 1){} //since the first byte read is a status code where 1 means the read was successful if the data is anything other than that the data is corruoted and useless 
+    if (data != 1) {} //since the first byte read is a status code where 1 means the read was successful if the data is anything other than that the data is corrupted and useless
 
-    if(sSerial.available() > 0) {
-      Serial.print("1st:  ");
-      Serial.print(sSerial.read(), HEX); //Prints first byte read 
+    if (sSerial.available() > 0) {
+      Serial.print(" 1st:  ");
+      Serial.print(sSerial.read(), HEX); //Prints first byte read
     }
 
-    if(sSerial.available() > 0) {
-      Serial.print("2nd:  ");
+    if (sSerial.available() > 0) {
+      Serial.print(" 2nd:  ");
       Serial.print(sSerial.read(), HEX); //Prints second byte read
     }
 
-    if(sSerial.available() > 0) {
-      Serial.print("3rd:  ");
+    if (sSerial.available() > 0) {
+      Serial.print(" 3rd:  ");
       Serial.print(sSerial.read(), HEX); //Prints third byte read
     }
 
-    if(sSerial.available() > 0) {
-      Serial.print("4th:  ");
+    if (sSerial.available() > 0) {
+      Serial.print(" 4th:  ");
       Serial.println(sSerial.read(), HEX); //Prints fourth byte read
       Serial.println("EOF");
     }
   }
 
-  delay(1000); 
+  delay(750);
 }
-
-
